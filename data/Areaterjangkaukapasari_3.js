68f61d799dab6717113d4f28a81208a95fe28019
auliarahmatka <auliarahmatka@gmail.com>
2026-05-21 18:07:17 +0700

    Add files via upload

---
+++ data/Areaterjangkaukapasari_3.js
@@ -0,0 +1,1 @@
+var json_Areaterjangkaukapasari_3 = {"type":"FeatureCollection","name":"Areaterjangkaukapasari_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
